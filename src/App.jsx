1afaf4403adccbadf0e5ea0686136564d674a9e3
amr4356 <amrooo73321@gmail.com>
2024-08-16 06:05:32 +0300

    511. Managing Multi-Component State with the Context API

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ function App() {
       <section>
         <h2>Why work with us?</h2>
         <Accordion className="accordion">
-          <AccordionItem className='accordion-item' title="We got 20 years of experience">
+          <AccordionItem id="experience" className='accordion-item' title="We got 20 years of experience">
             <article>
               <p>You can&apos;t got wrong with us.</p>
               <p>We are in the business of planning highly individualized vacation trips for more than 20 years.</p>
             </article>
           </AccordionItem>
-          <AccordionItem className="accordion-item" title="We're working with loacal guides">
+          <AccordionItem id="local-guides" className="accordion-item" title="We're working with loacal guides">
             <article>
               <p>we are not doing this along form our office.</p>
               <p>Instead, we are workin with local guides to ensure a safe and pleasant vacation </p>
